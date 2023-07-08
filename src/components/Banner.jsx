7e9af6d9f5cd084027c94b8e6b346eb2dc33e321
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2023-07-08 12:23:31 -0500

    Adjust View

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,34 +1,24 @@
-import React from "react"
-import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit"
+import React, { useState, useEffect } from "react"
 import "@styles/Banner.css"
-import bann1 from "@assets/b2.png"
-import bann2 from "@assets/b1.png"
-import bann3 from "@assets/bann3.png"
 
 const Banner = () => {
+
   return (
-    <div className="banner align-items-center d-flex">
-      <MDBCarousel showControls>
-        <MDBCarouselItem
-          className="bannerItem  m-auto"
-          itemId={1}
-          src={bann1}
-          alt="..."
-        />
-        <MDBCarouselItem
-          className="m-auto bannerItem "
-          itemId={2}
-          src={bann2}
-          alt="..."
-        />
-        <MDBCarouselItem
-          className=" m-auto bannerItem "
-          itemId={3}
-          src={bann3}
-          alt="..."
-        />
-      </MDBCarousel>
+    <div className="banner align-items-center ">
+      <div
+        className="color-change-5x"
+        
+      ></div>
+      <div
+        className="test  linew"
+        
+      >HOLA</div>
+      <div
+        className="color-change-5x1"
+        
+      ></div>
     </div>
+    
   )
 }
 
